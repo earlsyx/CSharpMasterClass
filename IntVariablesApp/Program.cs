6f15c3cbfb25fd6108290db66d2cd4e4576bf6cf
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 07:03:13 +0800

    Add project files.

---
+++ IntVariablesApp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// without decimal/whole nubmers
+int age = 0; //integer meaning whole number; explicit, implciit is 0
+
+//Int32 age = 0; UnInt32
+//Int64 age = 0; overkill. too large
+
+age = 43; // type do ont cross
+
+int ageInTenYears = age + 10;
+
+// Two Billion +/-
+// signed Int32, signed meaning + and - 
+// unsigned - 4 billion
+// bit - 0 or 1, computer used , gate open or close. 0 open 1 close , binary
+// byte - 8 bits - 00000000
+// 1, 11, 111, - doublede, multiplicatio nevy time you add 0.
+// 16 bit not twice as large, 9 bit is twice as large as 8 bit, 10 bit 4 tiems as large.
+// 32 bit, 32 zero's number of places we can store number in.
+// if larger, Int64 // not twice as much memory space
+// int 16, 2 bytes, int 32, 4 bytes, int 64, 8b ytes of data per number
+// 1 more bit double the size. 
+
+//Console.WriteLine(age.ToString());  //explicit
+//every object has special mehod attached to it.
+
+Console.WriteLine(age); //imp;lciit stills tring.
+Console.WriteLine(ageInTenYears);
+
+//asiginig new value, working with avalue.
+//don't divide int
+Console.WriteLine(age / 2);//careful with decimal, int result not decimal
+
---
+++ StringVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+// holds info, get initial value
+string firstName = string.Empty; //standard , initial value , define, declaration
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Midoriya"; // asign    
+lastName = "Izuku";
+//firstName = "123";
+//filePath = "C:\\Temp\\Demo \n"; // escapre character, to tell you are using special character \
+filePath = @"C:\Temp\Demo";//string literal
+
+//Console.WriteLine(firstName + " " + lastName); //print / /string literal.
+
+string testString = $@"The file for {firstName} is at C:\SampleFiles";
+
+//string interpolation
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine(filePath);
+Console.WriteLine(testString);
+
+//hungarianNotation ,type + camel case
