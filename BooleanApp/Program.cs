646c80cfdaa9a0d934f2a5eae06a1d434dc51b26
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 20:50:15 +0800

    boolean

---
+++ BooleanApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// True or False  /state
+
+bool isCompelete = false;  //blue because keyword, false default
+//evaluate things true or false.
+
+//isCompelete = !isCompelete;//flipping
+
+Console.WriteLine(!isCompelete);//modify outptui
+Console.WriteLine(isCompelete);
