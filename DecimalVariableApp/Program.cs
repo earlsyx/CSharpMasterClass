9c3a51927c1704ddb1c5ae2ee3c99ca112b69d1a
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 20:46:30 +0800

    decimal - use usually in money, needed more precision aroudn 28 positon, not use tat much, deobule is much prefered

---
+++ DecimalVariableApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+// 4.12 , 32 , 1.234
+// 4.12, 32, 1.234
+
+//cost benefit analywtsis , additonal cost, memory, reserve space emmeory, match larger spot
+// Int32 - 000000 000000 000000 000001
+
+decimal bankAccountBalance;  //28 position, decimal more precise,cmpare to double 15-16
+
+bankAccountBalance = 2.34M;
+
+Console.WriteLine(bankAccountBalance); //monay so no math function that uses decimal that much.
