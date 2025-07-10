e599c0c9df1a05ad600f1bed98df10560db735ba
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 21:27:21 +0800

    DateOnly , Date with time, DateOnly date only.

---
+++ DateOnlyVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿DateTime today = DateTime.Now;
+
+
+
+
+//backward and forward ocmpatbility, so DateOnly isnteadof Date
+DateOnly birthday = DateOnly.Parse(s: "6/11/1998");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Today just date: {birthday}");
