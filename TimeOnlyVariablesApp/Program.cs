a854a75bd773e633495a5e75cdd7495075d49e30
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 21:30:05 +0800

    TimeOnly

---
+++ TimeOnlyVariablesApp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+TimeOnly opensAt = TimeOnly.Parse(s: "8:00 AM");//timeonly
+
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(opensAt);
+Console.WriteLine(rightNow);
