1d57434f14136caa6f058e5967b4d95840be419e
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 20:59:34 +0800

    null - string can be null, different type,s, - nullabel settinig in project

---
+++ NullVariablesApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// not variable, but state a variable can be in.
+// null value can be assigne to any vrabile type.
+// - lack of value , there is nothing.
+
+// haven't asked for the age yet
+int? age = null; //default is 0 , not nullable, using ? type is nullable,//capacity t ostore null.
+//null keywrod, blackhole,none lack of vlaue.
+bool? birthDay = null;
+double? battingAverage = null;
+decimal? accountBalance = null;
+string? firstName = null;//works, different type struct. 
+// nullable setting in project/?. 
+
+// we have asked for age now
+age = 0;
