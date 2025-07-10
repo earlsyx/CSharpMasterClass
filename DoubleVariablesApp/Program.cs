6c47aea3b01d5885e62e13379d97a8bc309e4c93
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 20:40:00 +0800

    DoubleVariable - 15,16 places, - not precicse, rounding, efficent less signifcant digit, // use for ever praetion // exption mony

---
+++ DoubleVariablesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// doubles can have a decimal point - 1.23, 43,
+
+double averageAge;
+
+//averageAge = (43.0 + 21 + 62) / 3;
+
+averageAge = 2.0 / 3; // 15 , 16 places, not precise, some rounding everytime.
+//efficient because it has less signifcant digits, use for very operation, 
+// exception is money, because we want precision for money.
+
+Console.WriteLine(averageAge);
+
